cf7277ca701fb1e7b29b8931c0cb31e95f739d7c
tjcprince <tan_jianchao@163.com>
2016-01-13 10:27:17 +0800

    使用ui-navbar插件开发导航 修改导航为固定头部

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 eoms前端项目 gulp+bower+angular+bootstrap
 开发工具 sublime text 3.0
 执行 npm install && bower install
+
+1.插件ui-navbar不能自动引入css问题
+第一种方案
+(1)需要修改 bower_components/ui-navbar/bower.json中的"main" 为
+"main": ["release/js/ui-navbar.js","release/css/ui-navbar.scss"]
+(2)修改bower_components/ui-navbar/release/css/ui-navbar.css为ui-navbar.scss
+第二种方案
+把bower_components/ui-navbar/release/css/中的ui-navbar.css放到src/app中 并且修改名字为ui-navbar.scss
+暂时使用的是第一种方案
+
+
--- src/app/home/home.controller.js
+++ src/app/home/home.controller.js
@@ -9,12 +9,36 @@
 	function HomeController($log, $state) {
 		var vm = this;
 
-		vm.titleArray = [{
-			name: "首页",
-			id: 0
+		vm.tree = [{
+			name: "菜单",
+			link: "#",
+			subtree: [{
+				name: "菜单0",
+				link: "home.booklist({id:0})"
+			}, {
+				name: "菜单1",
+				link: "home.booklist({id:1})"
+			}]
 		}, {
-			name: "列表",
-			id: 1
+			name: "No states",
+			link: "#",
+			subtree: [{
+				name: "no state connected",
+				link: "#"
+			}]
+		}, {
+			name: "divider",
+			link: "#"
+
+		}, {
+			name: "State has not been set up",
+			link: "#"
+		}, {
+			name: "divider",
+			link: "#"
+		}, {
+			name: "Here again no state set up",
+			link: "#"
 		}];
 
 		$state.go("home.booklist", {
